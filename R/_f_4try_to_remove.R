f11ed275cdbed702e3e83ae43b4ef2fc093911df
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-04-03 16:36:09 +0200

    Update _f_4try_to_remove.R

--- R/_f_4try_to_remove.R
+++ R/_f_4try_to_remove.R
@@ -1,25 +1,6 @@
-aggmean <- function(X, y = NULL) {
+_f4 <- function(X, y = NULL) {
     
-    ## Works also if y is a factor
+    2 + 2
     
-    X <- .mat(X)
-    zdim <- dim(X)
-    n <- zdim[1]
-    p <- zdim[2]
-    colnam <- colnames(X)
-    
-    if(is.null(y)) 
-        y <- rep(1, n)
-
-    lev <-  sort(unique(y))
-    nlev <- length(lev)
-    ni <- c(table(y))
-    
-    ct <- matrix(nrow = nlev, ncol = p)
-    for(i in seq_len(nlev))
-        ct[i, ] <- colMeans(X[y == lev[i], , drop = FALSE])
-    dimnames(ct) <- list(lev, colnam)
-
-    list(ct = ct, lev = lev, ni = ni)
     
     }
