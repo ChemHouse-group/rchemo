9f5c9d3d45e2802a427b5f7ed930476a02d3a031
MarionBrandolini <123633895+MarionBrandolini@users.noreply.github.com>
2024-05-07 22:50:15 +0200

    Update soplsprobda.R
    case 0 lv

--- R/soplsprobda.R
+++ R/soplsprobda.R
@@ -18,16 +18,28 @@
     ## But same as:
     z <- fm[[1]]$T
     
-    fm[[2]] <- fun(z, y, prior = prior)
+    if(is.null(z)){
+      fm[[2]] <- list()
+      nlev <- length(zd$lev)
+      fm[[2]]$ct <- rep(0, nlev)
+      fm[[2]]$W <- NULL
+      fm[[2]]$wprior <- switch(prior,
+                               "unif" = rep(1 / nlev, nlev),
+                               "prop" = zd$ni / sum(zd$ni))
+      fm[[2]]$lev <- zd$lev
+      fm[[2]]$ni <- zd$ni
+    }else{
+      fm[[2]] <- fun(z, y, prior = prior)
+    }
     structure(list(fm = fm, lev = zd$lev, ni = zd$ni), 
               class = "Soplsprobda")       
 }
 
 soplslda <- function(Xlist, y, Xscaling = c("none", "pareto", "sd")[1], Yscaling = c("none", "pareto", "sd")[1], weights = NULL, nlv, prior = c("unif", "prop"))
-    .soplsprobda(Xlist, y, Xscaling = Xscaling, weights = weights, nlv = nlv, fun = rchemo::lda, prior = prior)
+    .soplsprobda(Xlist, y, Xscaling = Xscaling, Yscaling = Yscaling, weights = weights, nlv = nlv, fun = rchemo::lda, prior = prior)
 
 soplsqda <- function(Xlist, y, Xscaling = c("none", "pareto", "sd")[1], Yscaling = c("none", "pareto", "sd")[1], weights = NULL, nlv, prior = c("unif", "prop"))
-    .soplsprobda(Xlist, y, Xscaling = Xscaling, weights = weights, nlv = nlv, fun = rchemo::qda, prior = prior)
+    .soplsprobda(Xlist, y, Xscaling = Xscaling, Yscaling = Yscaling, weights = weights, nlv = nlv, fun = rchemo::qda, prior = prior)
 
 
 transform.Soplsprobda <- function(object, Xlist){
@@ -49,7 +61,12 @@ predict.Soplsprobda <- function(object, Xlist) {
     pred <- zres$pred
     posterior <- zres$posterior
   }else{
-    pred <- posterior <- NULL
+    posterior <- matrix(rep(object$fm[[2]]$wprior, each = nrow(Xlist[[1]])), 
+                        ncol = length(object$fm[[2]]$lev),
+                        dimnames = list(rownam, object$fm[[2]]$lev))
+    pred <- matrix(object$fm[[2]]$lev[which.max(object$fm[[2]]$wprior)[1]], 
+                   nrow = nrow(Xlist[[1]]),
+                   dimnames = list(rownam, colnam))
   }
 
   list(pred = pred, posterior = posterior)
