f23eeddd577bae7effe344d5c5ed462d73f5446d
MarionBrandolini <123633895+MarionBrandolini@users.noreply.github.com>
2026-05-05 16:44:32 +0200

    bug fix on the second function consensuspcanipals

--- R/consensuspcanipals2.R
+++ R/consensuspcanipals2.R
@@ -108,7 +108,7 @@ consensuspcanipals <- function(Xlist, blockscaling = TRUE, weights = NULL, nlv, 
 }
 
 
-transform.Consensuspcanipals <- function(object, X, nlv = NULL) {
+transform.Consensuspcanipals <- function(object, X, ..., nlv = NULL) {
   nlv <- if (is.null(nlv)) ncol(object$T) else min(nlv, ncol(object$T))
   
   X <- lapply(1:length(X), function(i) .mat(X[[i]]))
@@ -145,7 +145,7 @@ transform.Consensuspcanipals <- function(object, X, nlv = NULL) {
 }
 
 
-summary.Consensuspcanipals <- function(object, X) {
+summary.Consensuspcanipals <- function(object, X, ...) {
 
   X <- lapply(1:length(X), function(i) .mat(X[[i]]))
   Xinit <- X
